45e2add84faa4313038419f301942a50eb54a704
IgorSilvares <igorsilvares@gmail.com>
2023-09-04 22:16:27 +0200

    User Authentication

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,16 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.db import router
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from restaurant import views
-
-router = DefaultRouter()
-router.register('booking', views.BookingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
-    path('restaurant/menu/',include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
-
+    path("admin/", admin.site.urls),
+    path('restaurant/', include("restaurant.urls")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
